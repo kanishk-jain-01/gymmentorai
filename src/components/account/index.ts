0870dc48afe90c0fdbee8f334cf6e0e531cf8e25
Kanishk Jain <kanishk@gymmentorai.com>
2025-03-23 20:27:34 -0700

    Deleted Email List Functionality

--- src/components/account/index.ts
+++ src/components/account/index.ts
@@ -1,7 +1,6 @@
 export { default as AccountContent } from './AccountContent';
 export { default as AccountLoading } from './AccountLoading';
 export { default as DataPrivacySection } from './DataPrivacySection';
-export { default as EmailPreferencesSection } from './EmailPreferencesSection';
 export { default as NotificationBanner } from './NotificationBanner';
 export { default as SubscriptionWrapper } from './SubscriptionWrapper';
 export { default as UnitPreferencesSection } from './UnitPreferencesSection';
